9208d54fc67ba11209fbc4f4e14e0d570e0527bd
roquemrodrigo <roquemrodrigo@gmail.com>
2018-03-07 21:28:44 -0500

    comentario

--- src/AppBundle/Controller/RodrigoController.php
+++ src/AppBundle/Controller/RodrigoController.php
@@ -47,6 +47,37 @@ class RodrigoController extends Controller
         $helpers = $this->get(Helpers::class);
         return $helpers->json($data);
     }
+	
+    public function list2Action(Request $request)
+    {
+        // 1. hacer consulta con em Doctrine
+        $em = $this->getDoctrine()->getManager();
+        $dql = "SELECT a FROM BackendBundle:Aziende a ORDER BY a.id DESC";
+        $query = $em->createQuery($dql);
 
+
+        // 2. crear paginacion
+        $paginator = $this->get("knp_paginator");
+        $page = $request->query->getInt("page", 1); //  recibir parametro url app_dev.php/aziende/list?page=1
+        $items_per_page = 20;
+        $pagination = $paginator->paginate($query, $page, $items_per_page);
+        $total_items_count = $pagination->getTotalItemCount();//  obtener total
+
+
+        // 3. crear array con datos
+        $data = array(
+            "status" => "success",
+            "total_items_count" => $total_items_count,
+            "page_actual" => $page,
+            "items_per_page" => $items_per_page,
+            "total_pages" => ceil($total_items_count / $items_per_page),
+            "data" => $pagination
+        );
+
+
+        // 4. retornar datos en json
+        $helpers = $this->get(Helpers::class);
+        return $helpers->json($data);
+    }
 
 }
